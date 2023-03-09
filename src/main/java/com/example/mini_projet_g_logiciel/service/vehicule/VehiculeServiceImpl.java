97aecb8510ecd9e8f811f0bc55100c444d773da1
leeCoderEng <nouraddine.ait.elhadj@gmail.com>
2023-03-09 19:49:16 +0100

    vehicle service

--- src/main/java/com/example/mini_projet_g_logiciel/service/vehicule/VehiculeServiceImpl.java
+++ src/main/java/com/example/mini_projet_g_logiciel/service/vehicule/VehiculeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.mini_projet_g_logiciel.service.vehicule;
 
+import com.example.mini_projet_g_logiciel.dao.VehiculeRepository;
 import com.example.mini_projet_g_logiciel.entities.Vehicule;
 import org.springframework.stereotype.Service;
 
@@ -8,29 +9,41 @@ import java.util.List;
 @Service
 public class VehiculeServiceImpl implements VehiculeService{
 
+    private VehiculeRepository vehiculeRepository;
+
+    public VehiculeServiceImpl() {
+
+    }
+
+    public VehiculeServiceImpl(VehiculeRepository vehiculeRepository) {
+        this.vehiculeRepository = vehiculeRepository;
+    }
 
     @Override
     public List<Vehicule> getAllVehicles() {
-    return null;
+        return vehiculeRepository.findAll();
     }
 
     @Override
     public List<Vehicule> getAvailableVehicles() {
+        //TODO
         return null;
     }
 
     @Override
     public Vehicule getVehicleById(String vehicleId) {
-        return null;
+        Vehicule vehicule = vehiculeRepository.findById(vehicleId).orElse(null);
+        if (vehicule == null) throw new RuntimeException("No vehicle with the given ID");
+        return vehicule;
     }
 
     @Override
     public Vehicule saveVehicle(Vehicule vehicule) {
-        return null;
+        return vehiculeRepository.save(vehicule);
     }
 
     @Override
     public void removeVehicle(String vehicleId) {
-
+        vehiculeRepository.deleteById(vehicleId);
     }
 }
